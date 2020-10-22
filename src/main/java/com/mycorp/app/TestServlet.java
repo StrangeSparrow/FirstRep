a6993b29efe2d99d8ca0f209a6e66c1019b212b3
StrangeSparrow <73070412+StrangeSparrow@users.noreply.github.com>
2020-10-22 23:24:59 +0300

    Update TestServlet.java

--- src/main/java/com/mycorp/app/TestServlet.java
+++ src/main/java/com/mycorp/app/TestServlet.java
@@ -1,14 +1,17 @@
 package com.mycorp.app;
 
-import javax.servlet.*;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
-
-import java.io.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class TestServlet extends HttpServlet {
     public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter pw = response.getWriter();
+        pw.println("<H1>Me edit</H1>");
         pw.println("<B>Hello!");
         pw.close();
     }
