58eff89d3cf1e625a18598bf13fcf38bd54a6183
Ivan  Zalomin <ivan.zalomin@qligent.com>
2020-10-22 21:48:28 +0300

    Some fixes

--- src/main/java/com/mycorp/app/TestServlet.java
+++ src/main/java/com/mycorp/app/TestServlet.java
@@ -1,14 +1,15 @@
 package com.mycorp.app;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 
 import java.io.*;
 
 public class TestServlet extends HttpServlet {
-    public void service(ServletRequest request,ServletResponse response) throws ServletException, IOException {
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter pw = response.getWriter();
         pw.println("<B>Hello!");
-        pw. close () ;
+        pw.close();
     }
 }
