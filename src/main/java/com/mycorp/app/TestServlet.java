7b9a939abe052ec98af701da83bd517b1f12aae4
StrangeSparrow <73070412+StrangeSparrow@users.noreply.github.com>
2020-10-23 18:34:35 +0300

    Update TestServlet.java

--- src/main/java/com/mycorp/app/TestServlet.java
+++ src/main/java/com/mycorp/app/TestServlet.java
@@ -9,10 +9,16 @@ import java.io.PrintWriter;
 
 public class TestServlet extends HttpServlet {
     public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
+        response.setContentType("text/html;charset=UTF-8");
         PrintWriter pw = response.getWriter();
-        pw.println("<H1>Me edit</H1>");
-        pw.println("<B>Hello!");
+
+        ListNews listNews = new ListNews();
+        listNews.fillFromFile("webapps\\my-app-1.0-SNAPSHOT\\WEB-INF\\classes\\news.csv");
+
+        for(News news : listNews.getNewsList()) {
+            pw.println(news.creatForWeb());
+        }
+
         pw.close();
     }
 }
